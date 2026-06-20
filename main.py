ed61b9865c3ed2e35e26605680073cbc0311de4a
Marc Manatad <marc.manatad25@gmail.com>
2026-06-20 17:36:47 +0800

    replace individual player instances with updatable and drawable groups

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         pygame.Surface.fill(screen, color="black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
