9da9e37f487d31287ac25f8f29d286ae514d0d04
Marc Manatad <marc.manatad25@gmail.com>
2026-06-20 16:59:25 +0800

    add draw player function and parameters

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,12 +19,17 @@ def main():
         for event in pygame.event.get():
             pass
         pygame.Surface.fill(screen, color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return
+
+    
+
 
 if __name__ == "__main__":
     main()
